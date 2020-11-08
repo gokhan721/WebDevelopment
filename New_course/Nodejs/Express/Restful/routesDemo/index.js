5f3bcf59c9f103042b785a2968d18422b4ba435b
gokhan721 <simsek.gokhan721@gmail.com>
2020-11-08 09:38:17 +0300

    Method Override express

--- New_course/Nodejs/Express/Restful/routesDemo/index.js
+++ New_course/Nodejs/Express/Restful/routesDemo/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const methodOverride = require("method-override");
 const path = require("path");
 const { v4: uuid } = require("uuid");
 
@@ -9,10 +10,12 @@ app.use(express.urlencoded({ extended: true }));
 // for json
 app.use(express.json());
 
+app.use(methodOverride('_method'));
+
 app.set('view engine', 'ejs');
 app.set("views", path.join(__dirname, 'views'));
 
-const comments = [
+let comments = [
     {
         id: uuid(),
         username: 'Todd',
@@ -43,16 +46,36 @@ app.get("/comments/new", (req, res) => {
     res.render("comments/new");
 })
 
+app.get("/comments/:id", (req, res) => {
+    const { id } = req.params;
+    const comment = comments.find(c => c.id === id);
+    res.render("comments/show", { comment });
+})
+
 app.post("/comments", (req, res) => {
     const { username, comment } = req.body;
     comments.push({ username, comment, id: uuid() });
     res.redirect('/comments');
 })
 
-app.get("/comments/:id", (req, res) => {
+app.patch("/comments/:id", (req, res) => {
+    const { id } = req.params;
+    const { comment: newComment } = req.body;
+    const existsComment = comments.find(c => c.id === id);
+    existsComment.comment = newComment;
+    res.redirect("/comments");
+})
+
+app.get("/comments/:id/edit", (req, res) => {
     const { id } = req.params;
     const comment = comments.find(c => c.id === id);
-    res.render("comments/show", { comment });
+    res.render("comments/edit", { comment });
+})
+
+app.delete("/comments/:id", (req, res) => {
+    const { id } = req.params;
+    comments = comments.filter(c => c.id !== id);
+    res.redirect("/comments");
 })
 
 app.listen(3000, () => {
